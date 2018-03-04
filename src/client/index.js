5eaf804a2b233ac93e7880725e1b90f3cf9d3c09
Yury Karalkou <yury.karalkou@gmail.com>
2018-03-04 20:43:18 +0300

    [YK] Add full path to font

--- src/client/index.js
+++ src/client/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'typeface-open-sans';
+import 'typeface-open-sans/index.css';
 import registerServiceWorker from './registerServiceWorker';
 import Root from './components/Root';
 
