fbc20775cc9b8c91b6ed8192f664327921597ca6
Karalkou <yury.karalkou@gmail.com>
2018-03-06 10:10:45 +0300

    [YK] Move articles/blogs components from Start page to Admin page

--- src/client/components/routes/Admin.js
+++ src/client/components/routes/Admin.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import AddArticleForm from '../blog/AddArticleForm';
+import BlogListFiltrationPanel from '../blog/BlogListFiltrationPanel';
+import BlogList from '../blog/BlogList';
 
 class Admin extends Component {
     static propTypes = {
@@ -9,6 +12,9 @@ class Admin extends Component {
         return (
             <div>
                 <h2>Admin Page</h2>
+                <AddArticleForm/>
+                <BlogListFiltrationPanel/>
+                <BlogList/>
             </div>
         );
     }
--- src/client/components/routes/Start.js
+++ src/client/components/routes/Start.js
@@ -1,8 +1,5 @@
 import React, { Component } from 'react';
 import PageContainer from '../common/PageContainer';
-import AddArticleForm from '../blog/AddArticleForm';
-import BlogListFiltrationPanel from '../blog/BlogListFiltrationPanel';
-import BlogList from '../blog/BlogList';
 
 class Start extends Component {
     static propTypes = {};
@@ -11,9 +8,6 @@ class Start extends Component {
         return (
             <PageContainer>
                 <h2>Start Page</h2>
-                <AddArticleForm/>
-                <BlogListFiltrationPanel/>
-                <BlogList/>
             </PageContainer>
         );
     }
