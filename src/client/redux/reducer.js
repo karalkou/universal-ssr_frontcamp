311c56e3d489a38f1e6bafc2fa58033af63219ac
Yury Karalkou <yury.karalkou@gmail.com>
2018-03-08 15:28:43 +0300

    [YK] Activate auth logic in redux

--- src/client/redux/reducer.js
+++ src/client/redux/reducer.js
@@ -1,13 +1,14 @@
 import { combineReducers } from 'redux';
 import { routerReducer as router } from 'react-router-redux';
-import {reducer as form} from 'redux-form';
-// import authReducer, {moduleName as authModule} from '../ducks/auth'
+import { reducer as form } from 'redux-form';
+import authReducer, { moduleName as authModule } from '../ducks/auth';
 import articlesReducer, { moduleName as articlesModule } from '../ducks/articles';
 import filtersReducer, { moduleName as filtersModule } from '../ducks/filters';
 
 export default combineReducers({
-    router, form,
-    // [authModule]: authReducer,
+    router,
+    form,
+    [authModule]: authReducer,
     [articlesModule]: articlesReducer,
     [filtersModule]: filtersReducer,
 });
--- src/client/redux/saga.js
+++ src/client/redux/saga.js
@@ -1,9 +1,11 @@
 import { all } from 'redux-saga/effects';
 import { saga as articlesSaga } from '../ducks/articles';
+import { saga as authSaga } from '../ducks/auth';
 
 
 export default function* () {
     yield all([
         articlesSaga(),
+        authSaga(),
     ]);
 }
