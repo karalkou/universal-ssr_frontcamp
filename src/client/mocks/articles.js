8854f563436b50db76ff14a23cb698f744eb4fd3
Karalkou <yury.karalkou@gmail.com>
2018-03-13 17:28:57 +0300

    [YK] Create mocks directory. Move there mock articles. Update .gitignore

---
+++ src/client/mocks/articles.js
@@ -0,0 +1,30 @@
+export default [
+    {
+        createdAt: '2018-02-03T04:41:45.586Z',
+        updatedAt: '2018-02-03T04:41:45.586Z',
+        _id: '5a753d89aa7586161cef2403',
+        title: "Zhabinsky's birthday note",
+        author: 'Yury Karalkou',
+        views: 1,
+        body: "Zhabinsky's birthday was on 28.01.2018",
+        __v: 0,
+    },
+    {
+        createdAt: '2018-02-03T04:45:24.589Z',
+        updatedAt: '2018-02-03T04:45:24.589Z',
+        _id: '5a753e64ea59510b0ca6271c',
+        title: 'Documents (aka Objects)',
+        author: 'MLab',
+        views: 100500,
+        body: 'From the "Documents" tab you can browse and ' +
+        'search for objects in this collection. All standard ' +
+        'query constructs are supported except for map/reduce ' +
+        'queries. To use map/reduce, use the MongoDB shell ' +
+        '(note that temporary result collections will be ' +
+        'viewable in mLab). You can also add, edit, and ' +
+        'delete individual documents from here. Bulk collection ' +
+        'updates are not yet supported in this UI (although ' +
+        'they are supported in the shell).',
+        __v: 0,
+    },
+]
